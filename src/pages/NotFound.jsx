400cc60b17ba740fe9683f218288c1b3a2c5cbc3
Florian Gagnaire <gagnaire.flo@gmail.com>
2025-10-03 13:15:52 +0200

    Adding styles in not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,12 +1,19 @@
-import Bottom from "../components/bottom/Bottom";
-import Header from "../components/header/Header";
+import { Link } from "react-router";
+import Bottom from "@/components/bottom/Bottom";
+import Header from "@/components/header/Header";
 
 export default function NotFound() {
   return (
     <>
       <Header />
       <main>
-        <h1>404</h1>
+        <h1 className="notFound__title">404</h1>
+        <h6 className="notFound__subtitle">
+          Oups! La page que vous demandez n'existe pas.
+        </h6>
+        <Link to="/" className="notFound__link">
+          Retourner sur la page d'accueil
+        </Link>
       </main>
       <Bottom />
     </>
